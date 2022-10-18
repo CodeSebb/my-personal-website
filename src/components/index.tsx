9b097dc3c1aab2a5550243d58eeecf099d4d8c42
CodeSebb <SebastianSalvadorSanchez@gmail.com>
2022-10-17 20:18:18 -0700

    additional comments

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,10 @@ export { default as Links } from './links/Links';
 export { default as Dance } from './dance/Dance';
 export { default as Feature } from './feature/Feature';
 export { default as Navbar } from './navbar/Navbar';
+/*
+    The main decision behind this tsx doc in the containers folder was to increase modularity of the 
+    separate components that comprise the majority of this folder. It was also determined this was 
+    the design choice to allow separate components to have their own unique styling, along with  
+    ensuring the code readability improves.
+      
+*/
--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -3,3 +3,10 @@ export { default as Footer } from './footer/Footer';
 export { default as Header } from './header/Header';
 export { default as Code } from './code/Code';
 export { default as CodeSebb } from './CodeSebb/CodeSebb';
+/*
+    The main decision behind this tsx doc in the containers folder was to increase modularity of the 
+    separate components that comprise the majority of this folder. It was also determined this was 
+    the design choice to allow separate components to have their own unique styling, along with  
+    ensuring the code readability improves.
+      
+*/
